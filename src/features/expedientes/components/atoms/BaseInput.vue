86a98e643a06856b4825813cdd05fae7f385203f
Mar <mberrioz@unal.edu.co>
2025-06-09 17:01:54 -0500

    fix: improve search filters functionality - Remove number input spinners - Handle search errors gracefully - Implement partial search for all fields - Add debounce to search inputs

--- src/features/expedientes/components/atoms/BaseInput.vue
+++ src/features/expedientes/components/atoms/BaseInput.vue
@@ -1,13 +1,16 @@
 <template>
   <v-text-field
     :model-value="modelValue"
-    @update:modelValue="$emit('update:modelValue', $event)"
+    @update:modelValue="handleInput"
     :label="placeholder"
     outlined
     dense
-    hide-details
+    :type="type"
+    :error-messages="errorMessage"
+    :rules="rules"
     class="base-input"
     @input="updateValue"
+    :hide-spin-buttons="type === 'number'"
   ></v-text-field>
 </template>
 
@@ -26,14 +29,33 @@ export default {
     placeholder: {
       type: String,
       default: ''
+    },
+    rules: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      errorMessage: ''
     }
   },
   emits: ['update:modelValue', 'input'],
   methods: {
-    updateValue(event) {
-      const value = event.target.value;
+    handleInput(value) {
+      this.errorMessage = '';
+      if (this.type === 'number') {
+        if (value && !/^\d*$/.test(value)) {
+          this.errorMessage = 'Solo se permiten números';
+          return;
+        }
+      }
       this.$emit('update:modelValue', value);
       this.$emit('input', value);
+    },
+    updateValue(event) {
+      const value = event.target.value;
+      this.handleInput(value);
     }
   }
 }
@@ -66,5 +88,16 @@ export default {
 
 .base-input :deep(.v-field--focused .v-field__outline) {
   border-color: #fe4c00;
+}
+
+/* Ocultar flechas de input number */
+.base-input :deep(input[type="number"]::-webkit-inner-spin-button),
+.base-input :deep(input[type="number"]::-webkit-outer-spin-button) {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+.base-input :deep(input[type="number"]) {
+  -moz-appearance: textfield;
 }
 </style>
